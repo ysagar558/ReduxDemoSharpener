46578c9d35c606f43e975f4b702dd44563feada3
ysagar558 <ysagar558@gmail.com>
2026-01-10 19:23:47 +0530

    Redux demo

---
+++ redux-demo.js
@@ -0,0 +1,34 @@
+const redux=require('redux');
+
+const counterReducer=(state={counter:0},action)=>{
+if(action.type==='increment'){
+    return{
+        counter:state.counter+1
+    };
+}
+
+if(action.type==='decrement'){
+    return{
+        counter:state.counter-1
+    };
+}
+return state;
+};
+
+const store=redux.createStore(counterReducer);
+
+// console.log(store.getState());
+
+const counterSubscriber=()=>{
+    const latestState=store.getState();
+    console.log(latestState);
+}
+
+store.subscribe(counterSubscriber);
+for (let i=0;i<5;i++){
+store.dispatch({type:'increment'});
+}
+store.dispatch({type:'decrement'});
+
+
+
